3e162b65adbcf5a3bcffa3a44e690a98c867f217
Daniel Filipe Schwingel <danielfilipeschwingel@gmail.com>
2022-10-12 19:26:16 -0300

    Commit save

---
+++ src/components/Heading.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { clsx } from 'clsx'
+
+export interface HeadingProps {
+   size: 'sm' | 'md' | 'lg',
+   asChild?: true
+   children: ReactNode
+}
+
+export function Heading({ size = 'md', children, asChild }: HeadingProps) {
+   const Component = asChild ? Slot : 'h2'
+
+   return (
+      <Component 
+         className={clsx(
+            'text-gray-100 font-bold font-sans',
+            {
+               'text-lg': size === 'sm',
+               'text-xl': size === 'md',
+               'text-2xl': size === 'lg'
+            }
+         )}
+      >
+         {children}
+      </Component>
+   )
+
+}
---
+++ src/components/Text.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { clsx } from 'clsx'
+
+export interface TextProps {
+   size: 'sm' | 'md' | 'lg',
+   asChild?: true
+   children: ReactNode
+}
+
+export function Text({ size = 'md', children, asChild }: TextProps) {
+   const Component = asChild ? Slot : 'span'
+
+   return (
+      <Component 
+         className={clsx(
+            'text-gray-100 font-sans',
+            {
+               'text-xs': size === 'sm',
+               'text-sm': size === 'md',
+               'text-md': size === 'lg'
+            }
+         )}
+      >
+         {children}
+      </Component>
+   )
+
+}
